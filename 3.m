a4ac4c7f56eac630c014c5429962a43bf19e71ee
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-08-24 20:20:04 +0000

    implement swap

--- 3.m
+++ 3.m
@@ -1,4 +1,2 @@
-push 1
-pall
 swap
 pall
--- swap_m.c
+++ swap_m.c
@@ -1,12 +1,26 @@
 #include "monty.h"
 
+/**
+ * swap_m - add a new item on to the top a stack
+ * @stack: the topmost item on the stack:
+ * @line_number: the line from the .m file that is being executed
+ * Return: Nothing
+ */
 void swap_m(stack_t **stack, unsigned int line_number)
 {
 	stack_t *item_top = *stack;
-	stack_t *item_bottom = item_top->prev;
+	stack_t *item_bottom;
 	int temp;
 
-	if (item_top == NULL || item_bottom == NULL)
+	if (item_top == NULL)
+	{
+		dprintf(2, "L%u: can't swap, stack to short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	item_bottom = item_top->prev;
+
+	if (item_bottom == NULL)
 	{
 		dprintf(2, "L%u: can't swap, stack to short\n", line_number);
 		exit(EXIT_FAILURE);
